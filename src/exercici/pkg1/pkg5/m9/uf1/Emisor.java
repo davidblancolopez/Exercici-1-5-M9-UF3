a9ca78680f304e2d3cbfb44562fef3c6bda93d29
davidblancolopez <daviddbl94@gmail.com>
2017-05-03 20:07:26 +0200

    Cambios

--- src/exercici/pkg1/pkg5/m9/uf1/Emisor.java
+++ src/exercici/pkg1/pkg5/m9/uf1/Emisor.java
@@ -1,5 +1,8 @@
 package exercici.pkg1.pkg5.m9.uf1;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
@@ -9,7 +12,6 @@ import java.security.Signature;
 public class Emisor {
 
     private KeyPair keyPair;
-    private byte[] buffer;
 
     /**
      * Metodo que genera las claves que se utilizaran para cifrar y descifrar
@@ -30,12 +32,26 @@ public class Emisor {
      * @param priv
      * @return 
      */
-    public byte[] signData(byte[] data, PrivateKey priv) {
+    public byte[] signData(byte[] data, PrivateKey priv, File fitxer) {
         byte[] signature = null;
+        FileInputStream fis;
+        BufferedInputStream bis;
+        byte[] buffer = new byte [1024];
+        int mida;
+        
         try {
-
+            fis = new FileInputStream(fitxer);
+            bis = new BufferedInputStream(fis);
+            
             Signature signer = Signature.getInstance("SHA1withRSA");
             signer.initSign(priv); //Inicialitzem la firma digital a partir de l ’algorisme utilitzat
+            
+            while(bis.available() != 0){
+                mida = bis.read(buffer);
+                signer.update(buffer, 0, mida);
+            }
+            
+            
             signer.update(data); //Li assignem a l’objecte firma les dades a firmar digitalment
             signature = signer.sign(); //Finalment generem la firma.
         } catch (Exception ex) {
